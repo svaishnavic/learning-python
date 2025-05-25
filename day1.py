6839d5f5a63a30386c1de4bd3fc4b4d3730f7e1f
svaishnavic <cherukupallivaishnavi14@gmail.com>
2025-05-25 11:33:55 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,18 @@
+# DAY1
+#     install python 13.3
+#     Setup github and send repo details 
+#   Read PEP8 as musch as possible 
+
+#PROBLEM 
+#Define a,b,c,d variables with values 1, 2.3, "OK" and True 
+#print hello world with a once , then with b and then with c and then with d 
+ 
+a=1
+print(a, "Hello world")
+b = 2.3
+print(b, "Hello world")
+c = "OK"
+print(c, "Hello world")
+d = True
+print(d, "Hello world")
+
