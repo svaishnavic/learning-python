a02752051ea7b59ccc734b884dac4eda18b3ace8
Vaishnavi Cherukupalli <cherukupallivaishnavi14@gmail.com>
2025-05-27 09:05:26 +0530

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,13 @@
+
+a = 10+ 2j
+b = 1 + 1j
+print(a+b, a-b, a*b, a/b)
+name = "karthik"
+age = 45
+salary = 400
+"karthik" == "karthik"
+print( name == "xyz")
+print( salary <= 200)
+print( name == "abc" and age <= 20)
+print(name == "abc" or age >= 20)
+
