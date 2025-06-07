3b9c5ef54a58ad6252b709bd16a0717a10f7b2a1
Vaishnavi Cherukupalli <cherukupallivaishnavi14@gmail.com>
2025-06-07 17:16:12 +0530

    Delete problem2.py

--- problem2.py
+++
@@ -1,9 +0,0 @@
-name = input("Give name:")
-age = input("Give age:")
-age= int(age)
-if name == "xyz" and age < 40: 
-    print("Suitable")
-if age > 50 :
-    print("Old")
-if name != "xyz" :
-     print("not known")
