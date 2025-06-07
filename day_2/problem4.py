1652ecff11c6c5b5781ddc952c741b025a4b7331
Vaishnavi Cherukupalli <cherukupallivaishnavi14@gmail.com>
2025-06-07 18:57:41 +0530

    problem4.py

--- day_2/problem4.py
+++ day_2/problem4.py
@@ -1,5 +1,5 @@
 for x in range ( 101): 
-    for  y in range (101) :
-        for z in range (101) :
+    for  y in range (x,101) :
+        for z in range (y,101) :
             if z*z == x*x +  y*y :
                 print( x , y , z )
