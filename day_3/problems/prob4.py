5446053d65b1cab7f79b8e56b7bf63e4aa2f89eb
Vaishnavi Cherukupalli <cherukupallivaishnavi14@gmail.com>
2025-06-24 17:11:28 +0530

    prob4.py

--- day_3/problems/prob4.py
+++ day_3/problems/prob4.py
@@ -1,14 +1,15 @@
 lst = ["OK", "NOK", "hello", "hi"]
 even_strlength = 0
 odd_strlength = 0
-total_strlength = 0
+lengths = []
 
 for i in lst :
-    if len(i) %2 == 0 :
+    length = len(i)
+    if length %2 == 0 :
         even_strlength += 1
     else:   
         odd_strlength += 1
-    total_strlength += len(i)
+    total_strlength = sum(lengths)
 print(f"num of even lengths = {even_strlength}")
 print( f"num of odd lenghts = {odd_strlength}")
 print(f"total length of all strings = {total_strlength}")
