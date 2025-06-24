7843977ea939824180d980465c18af46a634342a
Vaishnavi Cherukupalli <cherukupallivaishnavi14@gmail.com>
2025-06-24 16:54:23 +0530

    prob3.py

--- day_3/problems/prob3.py
+++ day_3/problems/prob3.py
@@ -1,9 +1,7 @@
 import random 
 lst = []
-sum_numbers = 0
 for i in range(10) :
     rn=random.randint(10,50+1)
     lst.append(rn)
-    sum_numbers += rn
 print(lst)
-print(f"sum = {sum_numbers}")
+print(f"sum = {sum(lst)}")
