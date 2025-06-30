b8819742e92811789127318c608c30674f3643ee
Vaishnavi Cherukupalli <cherukupallivaishnavi14@gmail.com>
2025-06-30 22:53:20 +0530

    prob4.py

--- day_4/problems/prob4.py
+++ day_4/problems/prob4.py
@@ -1,3 +1,10 @@
 input = 'Name:ABC,age=20|Name:XYZ,age=30'
-input = input.replace('ABC','Abc').replace('XYZ','Xyz')
-print(input)
+spliting = input.split('|')
+print(spliting)
+output= []
+for i in spliting:
+    p,q = i.split(':')
+    r,s = q.split(',')
+    output.append(f"{p}:{r.capitalize()},{s}")
+result = '|'.join(output)
+print(result)
