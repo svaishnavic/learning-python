4791b66cdb92d829c962cf947a8a4083fa52ba37
Vaishnavi Cherukupalli <cherukupallivaishnavi14@gmail.com>
2025-06-30 19:44:56 +0530

    prob9.py

--- day_4/problems/prob9.py
+++ day_4/problems/prob9.py
@@ -1,8 +1,8 @@
-sum = {}
+triples = {}
 for x in range(101):
     for y in range(x,101):
         for z in range(y, 101):
             if z*z == x*x + y*y :
-                sum[(x,y,z)] = x+y+z
-for num,total in sum.items():
+                triples[(x,y,z)] = sum((x,y,z))
+for num,total in triples.items():
     print(f"{num} = {total}")
